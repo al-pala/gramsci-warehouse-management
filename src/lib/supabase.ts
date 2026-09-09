95da5e8f1d0bc0a22b4405ed7ef0e928ddd78703
al-pala <andrealuigipala@gmail.com>
2026-09-10 01:48:08 +0200

    Fix Supabase build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-export const supabase = createClient('https://tjksdchhnljpfhprozgy.supabase.co', 'sb_publishable_pT0NL8G40ziZf1xo_39kRQ_YIeV265T')
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+)
